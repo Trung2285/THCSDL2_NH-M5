a22c65bd089c4a57289c200fb31b6b0b2b4b4e45
vpyy <phucpro170305@gmail.com>
2024-11-06 07:46:45 +0700

    Update an_tuan8.sql

--- an_tuan8.sql
+++ an_tuan8.sql
@@ -1,8 +1,16 @@
 
 --b) Tăng số lượng hàng của những mặt hàng do công ty VINAMILK cung cấp lên gấp đôi.
-update MatHang
-set soLuong = soLuong * 2
-where CTNo = N'CT0003';
+UPDATE NhaCungCap
+SET maCT = 'VNM'
+WHERE maCT = 'CT0001';
+
+UPDATE NhaCungCap
+SET tenCongTy = 'VINAMILK'
+WHERE tenCongTy = N'Nhà Cung Cấp A';
+
+UPDATE MatHang
+SET soLuong = soLuong * 2
+WHERE CTNo = 'VNM';
 
 --c) Cập nhật giá trị của trường NOIGIAOHANG trong bảng DONDATHANG bằng địa chỉ của
 --khách hàng đối với những đơn đặt hàng chưa xác định được nơi giao hàng (giá trị trường NOIGIAOHANG bằng NULL).

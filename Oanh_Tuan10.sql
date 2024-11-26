fce8397ed76a167178e5c77d612483afd1579a9f
Nguyenngockieuoanh <nguyenngockieuoanh0902@gmail.com>
2024-11-26 20:52:56 +0700

    Create Oanh_Tuan10.sql

---
+++ Oanh_Tuan10.sql
@@ -0,0 +1,23 @@
+﻿--12. Hãy cho biết có những khách hàng nào lại chính là đối tác cung cấp hàng của công ty 
+--(tức là có cùng tên giao dịch).
+update KhachHang
+set tenGiaoDich=N'Giao Dịch XXX'
+where tenGiaoDich=N'Giao Dịch J';
+
+SELECT kh.*, ncc.*
+FROM KhachHang kh
+INNER JOIN NhaCungCap ncc ON kh.tenGiaoDich = ncc.tenGiaoDich;
+
+
+-- 14. Những đơn hàng nào yêu cầu giao hàng ngay tại công ty đặt hàng và những đơn đó là của công ty nào
+update DonDatHang 
+set noiGiaoHang=N'Hà Tĩnh'
+where DonDatHang.noiGiaoHang=N'Nghệ An'
+
+update DonDatHang 
+set noiGiaoHang=N'Quảng Trị'
+where DonDatHang.noiGiaoHang=N'Thanh Hóa'
+
+select ddh.*, ct.maCT, ct.tenCongTy, ct.diaChi
+from DonDatHang ddh, NhaCungCap ct, MatHang mh, ChiTietDatHang ctdh
+where ct.maCT=mh.CTNo and mh.maHang=ctdh.MHNo and ctdh.HDNo=ddh.soHoaDon and ddh.noiGiaoHang=ct.diaChi
